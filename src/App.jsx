5a69fafc9b61a4f2a8c01eb08e5a6e8dbf714551
Manhar Hadiyal <manharwebdev@gmail.com>
2025-12-10 15:18:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-500 underline">
+        Environment Setup Successful!
+      </h1>
+    </div>
+  )
+}
